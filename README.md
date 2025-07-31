305fab9d12b63247eef91999e3d649a961985349
QuocAn <doquocanhsr1@gmail.com>
2025-07-31 10:17:44 +0700

    2

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-Sử dụng các kỹ thuật học có giám sát để tự động hóa quy trình phê duyệt thẻ tín dụng cho các ngân hàng.
+# Đề bài:
+**Sử dụng các kỹ thuật học có giám sát để tự động hóa quy trình phê duyệt thẻ tín dụng cho các ngân hàng.**
 
-Tiền xử lý dữ liệu và áp dụng các kỹ thuật học có giám sát để tìm ra mô hình và tham số tốt nhất cho công việc. Lưu điểm chính xác từ mô hình tốt nhất của bạn dưới dạng biến số. best_scoreMục tiêu là điểm chính xác ít nhất là 0.75. Biến mục tiêu là cột cuối cùng của DataFrame.
+**Tiền xử lý dữ liệu và áp dụng các kỹ thuật học có giám sát để tìm ra mô hình và tham số tốt nhất cho công việc. Lưu điểm chính xác từ mô hình tốt nhất của bạn dưới dạng biến số. best_scoreMục tiêu là điểm chính xác ít nhất là 0.75. Biến mục tiêu là cột cuối cùng của DataFrame.**
+
+# Các bước tiền xử lý dữ liệu:
+- **Xử lý missing data: dùng replace() chuyển sang nan, thay bằng giá trị xuất hiện nhiều nhất (object) và mean() median() với numeric, one-hot coding, trích xuất, chia train:test và chuẩn hóa dữ liệu**
+
+# Decision Tree:
+**Là mô hình ra quyết định theo kiểu hỏi - đáp, liên tục đặt ra những câu hỏi cho đến khi tìm được câu trả lời**
+
+# Logistic Regression:
+**Dùng để dự đoán xác suất một mẫu thuộc về một lớp nào đó**
+
+**Bước 1: Tính tổng trọng số**
+
+**Bước 2: Chạy qua hàm sigmoid**
+
---
+++ credit-decision-tree.py
@@ -0,0 +1,97 @@
+# Import necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+
+# Load the dataset
+cc_apps = pd.read_csv("cc_approvals.data", header=None) 
+cc_apps.head()
+
+# Giá trị missing
+missing_value = ['0']
+
+# Các giá trị bạn muốn thử cho từng tham số (cho Decision Tree)
+max_depth_values = [3, 5, 7, 10, None]
+min_samples_split_values = [2, 5, 10]
+min_samples_leaf_values = [1, 2, 4]
+criterion_values = ['gini', 'entropy']
+
+# Chuyển missing value thành nan
+cc_apps_replace = cc_apps.replace(missing_value, np.nan)
+
+# copy
+apps_input = cc_apps_replace.copy()
+
+#xử lý missing value
+for col in apps_input.columns:
+    if apps_input[col].dtype == 'object':
+        most_freq = apps_input[col].value_counts().index[0] #Dùng gtri xuất hiện nhiều nhất
+        apps_input[col].fillna(most_freq, inplace=True) # Hàm fillna là hàm thay thế giá trị nan thành giá trị most_freq
+    else:
+        mean_value = apps_input[col].mean() # Dùng giá trị trung bình mean(), trung vị median() cho dữ liệu numeric
+        apps_input[col].fillna(mean_value, inplace=True)
+
+# Biến đổi dạng chuỗi phân loại thành nhị phân (0, 1)
+apps_input = pd.get_dummies(apps_input, drop_first=True)
+
+#Hàm iloc dùng để trích xuất dữ liệu
+X = apps_input.iloc[:, :-1].values
+y = apps_input.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.4, random_state=42
+)
+
+scaler = StandardScaler()
+
+X_train_scaler = scaler.fit_transform(X_train)
+X_test_scaler = scaler.transform(X_test)
+
+model = DecisionTreeClassifier()
+
+model.fit(X_train_scaler, y_train)
+
+y_pred = model.predict(X_test_scaler)
+
+cm = confusion_matrix(y_test, y_pred)
+
+print("Confusion Matrix:\n", cm)
+
+acc = (cm[1][1] + cm[0][0]) / (cm[1][1] + cm[0][0] + cm[1][0] + cm[0][1])
+precision = cm[1][1] / (cm[1][1] + cm[0][1])
+recall = cm[1][1] / (cm[1][1] + cm[1][0])
+f1_score = 2 * ((precision*recall)/(precision+recall))
+
+# Dsach tham so cho Decision Tree
+param_grid = dict(
+    max_depth=max_depth_values,
+    min_samples_split=min_samples_split_values,
+    min_samples_leaf=min_samples_leaf_values,
+    criterion=criterion_values
+)
+
+# Lưới test
+grid_search = GridSearchCV(estimator=model, param_grid=param_grid, 
+                           cv=5, scoring='accuracy', n_jobs=-1)
+
+grid_search.fit(X_train_scaler, y_train)
+
+best_model = grid_search.best_estimator_
+
+best_score = best_model.score(X_test_scaler, y_test)
+
+print(best_score)
+
+
+
+
+
+
+
+
+
+
